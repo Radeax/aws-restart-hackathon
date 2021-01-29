546366e4c99f01c09af481efc8f0c0693f80237d
radeax <joflip@gmail.com>
2021-01-29 15:01:54 -0500

    move database info to env file

--- connection.py
+++ connection.py
@@ -1,10 +1,15 @@
+# pip install python-dotenv
 import mysql.connector
+from dotenv import load_dotenv
+import os
+from os import getenv
+load_dotenv()
 
 
 def getConnection():
     return mysql.connector.connect(
-        host="",  # HOSTNAME
-        user="",
-        passwd="",
-        database=""
+        host=getenv('DB_URL'),
+        user=getenv('DB_USER'),
+        password=getenv('DB_PASSWORD'),
+        database=getenv('DB_NAME')
     )
